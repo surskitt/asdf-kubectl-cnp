de72cfb1d0449ddf5130514ac58db7b81c758127
surskitt <surskitt@users.noreply.github.com>
2025-03-03 15:31:41 +0000

    feat: initial templated plugin

--- contributing.md
+++ contributing.md
@@ -5,7 +5,6 @@ Testing Locally:
 ```shell
 asdf plugin test <plugin-name> <plugin-url> [--asdf-tool-version <version>] [--asdf-plugin-gitref <git-ref>] [test-command*]
 
-# TODO: adapt this
 asdf plugin test kubectl-cnp https://github.com/surskitt/asdf-kubectl-cnp.git "kubectl-cnp version"
 ```
 
--- lib/utils.bash
+++ lib/utils.bash
@@ -2,7 +2,6 @@
 
 set -euo pipefail
 
-# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for kubectl-cnp.
 GH_REPO="https://github.com/EnterpriseDB/kubectl-cnp"
 TOOL_NAME="kubectl-cnp"
 TOOL_TEST="kubectl-cnp version"
@@ -27,12 +26,10 @@ sort_versions() {
 list_github_tags() {
 	git ls-remote --tags --refs "$GH_REPO" |
 		grep -o 'refs/tags/.*' | cut -d/ -f3- |
-		sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
+		sed 's/^v//'
 }
 
 list_all_versions() {
-	# TODO: Adapt this. By default we simply list the tag names from GitHub releases.
-	# Change this function if kubectl-cnp has other means of determining installable versions.
 	list_github_tags
 }
 
@@ -41,8 +38,9 @@ download_release() {
 	version="$1"
 	filename="$2"
 
-	# TODO: Adapt the release URL convention for kubectl-cnp
-	url="$GH_REPO/archive/v${version}.tar.gz"
+	system="$(uname -s | tr '[:upper:]' '[:lower:]')_$(uname -m)"
+
+	url="${GH_REPO}/releases/download/v${version}/kubectl-cnp_${version}_${system}.tar.gz"
 
 	echo "* Downloading $TOOL_NAME release $version..."
 	curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
@@ -61,7 +59,6 @@ install_version() {
 		mkdir -p "$install_path"
 		cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
 
-		# TODO: Assert kubectl-cnp executable exists.
 		local tool_cmd
 		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
 		test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
